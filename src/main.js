8d4d49da695196b6a89eb838d172b6ca1b55d472
fabio-susinn <fabiosusinp@gmail.com>
2024-10-05 15:58:52 +0000

    BOOTSRAP DEPENDENCY ADDED AND PACKAGE FILES UPDATED

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.js'
+
+createApp(App).use(bootstrap).mount('#app')
