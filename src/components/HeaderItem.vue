c00d005b496224164a26c23973f4baa0ee8331b3
fabio-susinn <fabiosusinp@gmail.com>
2024-10-06 16:25:39 +0000

    FRONTEND TRACKING SURVEY & SUBMIT BUTTON

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Student Syndrome</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                ESP
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">ESP</a></li>
+                <li><a class="dropdown-item" href="#">EN</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@ import './assets/main.css'
 import App from './App.vue'
 
 import { createApp } from 'vue'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 
-createApp(App).use(bootstrap).mount('#app')
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  locale: 'es',
+  fallbackLocale: 'en',
+  messages: {
+    en: {
+      message: {
+        hello: 'hello world'
+      }
+    },
+    es: {
+      message: {
+        hello: 'hola mundo'
+      }
+    }
+  }
+})
+
+createApp(App).use(bootstrap).use(i18n).mount('#app')
