c6ca7d6b778fe837736b2255c1e080fe43e71d5c
fabio-susinn <fabiosusinp@gmail.com>
2024-10-24 18:10:02 +0000

    CHANGED TO MOUNTED

--- src/components/FormView.vue
+++ src/components/FormView.vue
@@ -121,7 +121,7 @@ document.cookie = "__vercel_live_token=value; SameSite=None; Secure";
                     v-model="percentage[n - 1]"
                     step="10"
                     :name="`percentage-d${n}`"
-                  
+                    @input="updateOtherSliders(n - 1, percentage[n - 1])"
                   />
                 </div>
                 <div class="col">{{ percentage[n - 1] }}%</div>
@@ -155,7 +155,7 @@ export default {
       dedicationValues: Array(5).fill(null)
     }
   },
-  beforeMount() {
+  mounted() {
     this.dataUser()
     this.computeSubjects()
   },
